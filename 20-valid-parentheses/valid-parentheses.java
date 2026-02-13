dec7ce885581e6024544cd37226e083914c1a79b
Shouvik Dutta <142233626+Shouvik-9@users.noreply.github.com>
2026-02-13 21:16:48 +0530

    Time: 3 ms (87.58%) | Memory: 43.2 MB (85.52%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -3,19 +3,14 @@ class Solution {
         Stack<Character> stack = new Stack<>();
 
         for (char ch : s.toCharArray()) {
-
-            // If opening bracket, push
             if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
             } 
-            // If closing bracket
             else {
                 if (stack.isEmpty()) {
                     return false;
                 }
-
                 char top = stack.pop();
-
                 if ((ch == ')' && top != '(') ||
                     (ch == '}' && top != '{') ||
                     (ch == ']' && top != '[')) {
